bcdfba595ff3a0bf70421738c0709dcd6a9ccb95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 13:03:55 +0300

    Set up the routes for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
-	return <></>
+	return (
+		<Router>
+			<Header />
+			<main className='container content'>
+				<Routes>
+					<Route path='/' element={<HomeScreen />} />
+					<Route path='/login' element={<LoginScreen />} />
+					<Route path='/register' element={<RegisterScreen />} />
+					<Route path='/user-profile' element={<ProfileScreen />} />
+				</Routes>
+			</main>
+		</Router>
+	)
 }
 
 export default App
