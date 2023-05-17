ce9b42a1543b337b1faac6c821bc664815916c4d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 13:13:42 +0300

    Wrote the registerUser async thunk

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,6 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
+const backendURL = 'http://127.0.0.1:8000'
+
+export const registerUser = createAsyncThunk(
+	'auth/register',
+	async ({ firstname, email, password }, { rejectedValue }) => {
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+
+			await axios.post(
+				`${backendURL}/api/users/register`,
+				{ firstname, email, password },
+				config
+			)
+		} catch (error) {
+			if (error.response && error.response.data.message) {
+				return rejectedValue(error.response.data.message)
+			} else {
+				return rejectedValue(error.message)
+			}
+		}
+	}
+)
+
 const initialState = {
 	loading: false,
 	userInfo: {},
